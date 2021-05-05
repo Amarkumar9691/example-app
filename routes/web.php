e136a861f17e40d86c9e357773b71e5ed30405b2
Amarkumar9691 <83120077+Amarkumar9691@users.noreply.github.com>
2021-05-05 07:22:51 -0500

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -21,11 +21,14 @@ Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 
-
+/*Route for display adding task form*/
 Route::get('add_task_form',[App\Http\Controllers\TaskController::class,'add_task_form'])->name('add-task-form')->middleware('auth');
+
+/*Route to fetch record from tasklist and display in update form*/
 Route::get('taskupdateform/{id}',[App\Http\Controllers\TaskController::class,'edit_task_form'])->name('edit-task-form')->middleware('auth');
+
+/*Route to delete Task from tasklist*/
 Route::delete('taskremove/{id}',[App\Http\Controllers\TaskController::class,'task_remove'])->name('task-remove')->middleware('auth');
-Route::get('taskupdate/{id}',[App\Http\Controllers\TaskController::class,'update_task'])->name('update-task')->middleware('auth');
 
 
 Route::resource('profile',App\Http\Controllers\ProfileController::class)->middleware('auth');
